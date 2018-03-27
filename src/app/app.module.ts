71e1ff0be688645feae39881f148df475fe23afa
Kiran Joshi <Kiran.Joshi@Agilisys.co.uk>
2018-03-27 14:54:33 +0530

    Corporate : header & footer added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
+import { ArticleComponent } from './article/article.component';
+import { ResearchComponent } from './research/research.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ContactComponent,
+    HomeComponent,
+    ArticleComponent,
+    ResearchComponent
   ],
   imports: [
     BrowserModule
